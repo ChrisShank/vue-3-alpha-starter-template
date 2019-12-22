722a6262ca37872e0718030cae2c5f8281e27fb3
chrisshank <chris.shank.23@gmail.com>
2019-12-22 18:34:08 -0500

    Added build script and added count component

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
-import { createApp, createComponent, h } from 'vue/dist/vue.esm'
+// We must use the esm distribution of Vue since has Vue's compiler
+// You should import everything that you use with Vue using 'vue/dist/vue.esm'
+import { createApp, createComponent, h, computed, ref, watch } from 'vue/dist/vue.esm'
 
-const App = createComponent({
-	template: 'Hello {{ foo }}',
-
+const Count = createComponent({
+	template: `
+	<button @click="count += 1">Increment Count</button>
+	<button @click="count -= 1">Decrement Count</button>
+	<div>Count: {{ count }}</div>
+	<div>Double Count: {{ doubleCount }}</div>
+	`,
 	props: {
-		foo: { required: true, type: String }
+		initialCount: { required: true, type: Number }
+	},
+	setup(props) {
+		const count = ref(props.initialCount)
+		const doubleCount = computed(() => count.value * 2)
+
+		watch(() => console.log(count.value))
+
+		return { count, doubleCount }
 	}
 })
 
-const vm = createApp().mount({ render: () => h(App, { foo: 'World' }) }, '#app')
+const app = createApp().mount({
+	render: () => h(Count, { initialCount: 2 }),
+}, '#app')
--- src/vue-shim.ts
+++ src/vue-shim.ts
@@ -1,3 +1,4 @@
+// This is how we provide typings for the ESM distribution of Vue
 declare module 'vue/dist/vue.esm' {
 	export * from 'vue'
 }
