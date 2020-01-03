8b815fec0244b257f0e47c632f850477c2aa7e5d
chrisshank <chris.shank.23@gmail.com>
2020-01-03 18:12:02 -0500

    updated to vue 3 alpha 1

--- src/components/count.ts
+++ src/components/count.ts
@@ -1,17 +1,14 @@
-import { createComponent, ref, computed, watch } from 'vue'
+import { defineComponent, ref, computed, watch } from 'vue'
 
-export const Count = createComponent({
+export const Count = defineComponent({
 	template: `
 	<button @click="count += 1">Increment Count</button>
 	<button @click="count -= 1">Decrement Count</button>
 	<div>Count: {{ count }}</div>
 	<div>Double Count: {{ doubleCount }}</div>
 	`,
-	props: {
-		initialCount: { required: true, type: Number }
-	},
-	setup(props) {
-		const count = ref(props.initialCount)
+	setup() {
+		const count = ref(1)
 		const doubleCount = computed(() => count.value * 2)
 
 		watch(() => console.log(count.value))
